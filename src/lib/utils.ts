53015ad37533d7d23c11cca3a6d965863a141d1e
Vixeln <vireakcny@gmail.com>
2025-08-02 17:15:04 -0700

    Initialized shadcn Note to self (@Vixeln): I'm very dubious about the import aliases pointing to already aliased paths in `components.json`

---
+++ src/lib/utils.ts
@@ -0,0 +1,11 @@
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+/**
+ * This function was automatically generated from shadcn initialization
+ * 
+ * @author Vireak
+ */
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
