bd2c00bfceec2629092c583b8813a2e963c32cee
Vixeln <vireakcny@gmail.com>
2025-08-02 17:35:28 -0700

    Added extra documentation to `cn()` function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,6 +4,8 @@ import { twMerge } from "tailwind-merge"
 /**
  * This function was automatically generated from shadcn initialization
  * 
+ * 
+ * For more information on shadcn see https://ui.shadcn.com/docs. (I don't want to document a summary of what shadcn is right now)
  * @author Vireak
  */
 export function cn(...inputs: ClassValue[]) {
