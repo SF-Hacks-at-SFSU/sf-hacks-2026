0d5d031d24fac795db44942993f76e7e105b440a
Vixeln <vireakcny@gmail.com>
2025-10-06 12:38:45 -0700

    Split QnA accordions into 2 columns

--- src/app/faqs/components/Faqs.tsx
+++ src/app/faqs/components/Faqs.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 // import "../styles.css";
-import { qnas as data } from "@/data/faqs";
+import { qnas as qnaData } from "@/data/faqs";
 import {
 	Accordion,
 	AccordionContent,
@@ -8,34 +8,42 @@ import {
 	AccordionTrigger,
 } from "@/components/ui/accordion";
 
+const qnaColumnSecond = qnaData.splice(qnaData.length / 2);
+const qnaColumnFirst = qnaData;
+
+const qnaColumnGroups = [qnaColumnFirst, qnaColumnSecond];
+
 // TODO: Add class name support for the component
 function FAQ() {
 	return (
 		// TODO: try to simplify the tailwind after fixing global.css
-		<>
-			<Accordion
-				type="multiple"
-				// The transparent background property is jsut to override the global style for now. 
-				// It should be removed once global.css is properly cleaned up
-				className="basis-full p-0 rounded-none max-h-[65dvh] overflow-auto bg-[#0000]"
-			>
-				{data.map((question, questionIndex) => (
-					<AccordionItem
-						className="border-1 rounded-none max-w-(--paragraph-width-default)"
-						key={questionIndex}
-						value={questionIndex.toString()}
-					>
-						{/* The padding and corner radius are split into the Trigger and Content component so that their background colors can fill the whole accordion */}
-						<AccordionTrigger className="max-w-full rounded-none p-(--padding-default) bg-(--background-color-element-opaque)">
-							{question.question}
-						</AccordionTrigger>
-						<AccordionContent className="rounded-none p-(--padding-default) bg-(--background-color-element)">
-							{question.answer}
-						</AccordionContent>
-					</AccordionItem>
-				))}
-			</Accordion>
-		</>
+		// The transparent background property is just to override the global style for now.
+		// It should be removed once global.css is properly cleaned up
+		<div className="flex flex-row flex-wrap gap-x-(--rootChildMargin) p-0 rounded-none max-h-[65dvh] max-w-[2*calc(var(--paragraph-width-default))] overflow-auto bg-[#0000]">
+			{qnaColumnGroups.map((qnaColumnGroup, index) => (
+				<Accordion
+					key={`col-${index}`}
+					type="multiple"
+					className="w-(--paragraph-width-default)"
+				>
+					{qnaColumnGroup.map((question, questionIndex) => (
+						<AccordionItem
+							className="border-1 rounded-none"
+							key={questionIndex}
+							value={questionIndex.toString()}
+						>
+							{/* The padding and corner radius are split into the Trigger and Content component so that their background colors can fill the whole accordion */}
+							<AccordionTrigger className="max-w-full rounded-none p-(--padding-default) bg-(--background-color-element-opaque)">
+								{question.question}
+							</AccordionTrigger>
+							<AccordionContent className="rounded-none p-(--padding-default) bg-(--background-color-element)">
+								{question.answer}
+							</AccordionContent>
+						</AccordionItem>
+					))}
+				</Accordion>
+			))}
+		</div>
 	);
 }
 
